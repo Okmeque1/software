66e84e9b44620464c523efef09e115e068caabff
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-07-19 17:10:07 +0100

    New and Improved version

--- pwd_checker.py
+++ pwd_checker.py
@@ -1,3 +1,4 @@
+import time
 def check():
     risk = 0
     print("The Okmeque1 Password System Checker.")
@@ -5,6 +6,8 @@ def check():
     print("SPONSOR : This is perfect with Okmeque1's PWD.PY generator suite.")
     print("This program does NOT connect to the internet or external storage outside of this function.Please rest assured that this program does not steal information.")
     chkpwd = input("Please enter your password here → ")
+    print("Reading...Done.\nNow checking and evalutating...")
+    time.sleep(len(chkpwd) // 10)
     flag = True
     while flag == True:
         spc_cnt = 0
@@ -16,37 +19,38 @@ def check():
                 spc_cnt += 1
             if x.isnumeric() == True:
                 num_cnt += 1
-            if x.isalpha():
+            if x.isalpha() == True:
                 if x.isupper() == True:
                     up_cs += 1
                 if x.islower() == True:
                     lwc_cs += 1
-        if spc_cnt <= 1:
+        if spc_cnt <= 2:
             print("You must add more special characters in your password.")
             risk += 1
-        if num_cnt <= 1:
+        if num_cnt <= 2:
             print("You must add more numbers in your password.")
             risk += 1
-        if lwc_cs <= 1:
+        if lwc_cs <= 2:
             print("You must add more lowercase letters in your password.")
             risk += 1
-        if up_cs <= 1:
+        if up_cs <= 2:
             print("You must add more UPPERCASE letters in your password.")
             risk += 1
         if len(chkpwd) <= 10:
             print("Your password LENGTH must be longer in your password.")
             risk += 1
-        if len(chkpwd) - lwc_cs == 1:
+        if lwc_cs >= len(chkpwd) // 2:
             print("There are TOO MANY lowercase letters in your password.")
             risk += 1
-        if len(chkpwd) - up_cs == 1:
+        if up_cs >= len(chkpwd) // 2:
             print("There are TOO MANY UPPERCASE letters in your password.")
-            risk += 1            
+            risk += 1       
         print("Special Character count : " + str(spc_cnt))
         print("Number count : " + str(num_cnt))
         print("Lowercase letters : " + str(lwc_cs))
         print("Uppercase letters : " + str(up_cs))
         print("The risk of your account getting hacked is " + str(risk) + "/7")
+        exit = input("Press enter to exit.")
         flag = False
         return False
 check()      
