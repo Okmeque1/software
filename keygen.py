e6d3abb0cea6235f397a0565bfcc4aed2bcd8543
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-07-19 20:14:59 +0100

    KEYGEN - You need this for PWD-HASH_EDITION

--- keygen.py
+++ keygen.py
@@ -1,5 +1,6 @@
 from cryptography.fernet import Fernet
 def keygen():
+    print("Program made by Okmeque1")
     keygen1 = input("Please enter a valid file name to generate the key(Please make sure that the file is empty before use.) → ")
     key = Fernet.generate_key()
     with open(keygen1,"wb") as openfl:
