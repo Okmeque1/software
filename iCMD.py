6915292043473c49f93ce4664f19077957a222e5
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-08-15 12:43:51 +0200

    iCMD - Robust Error Handling - Extra commands

--- iCMD.py
+++ iCMD.py
@@ -1,31 +1,59 @@
-from tkinter import messagebox
-import tkinter as tk
 def icmd():
-    import os
-    iprompt = '>'
     try:
-        while True:
-            prompt = input(iprompt)
-            if prompt == 'exit':
-                print("Use 'return' to exit iCMD")
-            elif prompt == 'return':
-                return False
-            os.system(prompt)
-    except:
+        import os
+        iprompt = 'iCMD>'
+        ver = 'iCMD Full 1.03A - Okmeque1 Corporation - (c) Okmeque1 Corporation 2023-2025 All Rights Reserved.'
+        print(ver)
         try:
-            print("An exception has occured in function icmd().It may be possible to continue normally.")
-            print("Press ENTER <-| to ATTEMPT to continue")
-            print("Press CTRL-D/Z to terminate this program")
-            input("              Press any key to continue")
-            icmd()
+            while True:
+                prompt = input(iprompt)
+                if prompt == 'exit' or prompt == 'return':
+                    return False
+                elif prompt == 'ver':
+                    print(ver)
+                elif 'cd' in prompt[0:3]:
+                    os.chdir(prompt[3:])
+                elif prompt == 'dsc':
+                    print("iCMD - CMD for computers with restricted/no access to CMD.EXE or Linux/Mac Terminal")
+                    print("Use at your own risk - Okmeque1 Corporation is not responsible for any damages done to any computers or people using this program.")
+                    print("If iCMD gets blocked download the Lite version.")
+                    print("Commands depend on your system.The prompt can be changed from the iprompt value in the code")
+                    print("Commands may not work depending on your system.")
+                    print("Current system : " + os.name)
+                else:
+                    os.system(prompt)
+        except OSError:
+            print("STOP : The Operating System has forcibly closed the running process due to a fatal system error.")
+            input("Press ENTER to exit...")
+            return None
+        except ValueError:
+            print("STOP : The value for a variable is either invalid or an access violation has occured.")
+            input("Press ENTER to exit...")
+            return None
+        except PermissionError:
+            print("STOP : Access Violation has occured in your file.The Operating system will now forcibly close due to this error")
+            input("Press ENTER to exit...")
+            return None
+        except FileNotFoundError:
+            print("STOP : The requested file you specified does not exist.The Operating system will forcibly close the program due to reading in an invalid space.")
+            input("Press ENTER to exit...")
+            return None
+        except EOFError:
+            print("STOP : The requested operation read beyond the end of the specified file.The Operating system will now forcibly close the program.")
+            input("Press ENTER to exit...")
+            return None
+        except KeyboardInterrupt:
+            print("STOP : The user has chosen to exit.Exiting...")
+            return None
+        except IOError:
+            print("STOP : I/O error has occured.This means a device on your system has either malfunctioned or has been unplugged.The Operating system will now forcibly close the program.")
+            input("Press ENTER to exit.")
+            return None
         except:
-            a = tk.Tk()
-            a.withdraw()
-            e = messagebox.showwarning("Python","Python has caused an error in\nPYTHON.EXE.\nPython will now close.\n\nIf you continue to experience problems,\ntry restarting your computer.")
-            b = messagebox.showerror("PYTHON.EXE - Application error","PYTHON caused a general protection fault in KRNL386.EXE at 0001:8402")
-            c = messagebox.showerror("Error","String ERR_NOT_IMPLEMENTED not found in string table.")
-            d = messagebox.showerror("","ERR_NOT_IMPLEMENTED")
-            exit()
-
+            print("STOP : The program has forcibly exited with code 1")
+            input("Press ENTER to exit...")
+            return None
+    except:
+        print("STOP : EXCEPTION_IN_EXCEPTION : The program has attempted to recover from an exception causing another exception which caused it to forcibly close.")
+        return None
 icmd()
-        
