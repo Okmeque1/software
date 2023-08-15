f1295197b63fcb7c131f6b7765008c9361c7e2e7
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-08-15 12:42:55 +0200

    iCMD-Lite - Bug Fixes

--- iCMD-Lite.py
+++ iCMD-Lite.py
@@ -1,8 +1,11 @@
 import os
 def icmd():
+    print("iCMD-Lite - iCMD with limited features : core functionality.\nCopyright (c) Okmeque1 Corporation 2023-2025")
     while True:
-        prompt = input(">")
+        prompt = input("iCMD-Lite>")
         if prompt == 'exit' or prompt == 'return':
                 return False
+        elif 'cd' in prompt[0:3]:
+                os.chdir(prompt[3:])
         os.system(prompt)
 icmd()
