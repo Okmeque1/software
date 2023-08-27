c4367215d1453963f44d7bd343e5099302614d95
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-08-27 15:14:38 +0100

    HangMan - Now hangs man.

--- PythonSoft/hangman .py
+++ PythonSoft/hangman .py
@@ -1,5 +1,4 @@
 import random
-import os
 B = 1
 wrong = True
 print("HANG-MAN - Okmeque1 edition")
@@ -8,7 +7,15 @@ if file == "":
     file = "G:\python\demo\words.txt"
 fileopen = open(file,"r")
 ninety = []
-w = ["WRONG","No, just no","Swing and a miss!","Error, error. Bad answers.","Failure"]
+w = ["WRONG","No, just no","Swing and a miss!","Error, error. Bad answers.","Failure","May I suggest thinking?","Try harder please.","How about no?"]
+hf = [  [" "," "," "," "," "," "],
+        [" "," "," "," "," "," "],
+        [" "," "," "," "," "," "],
+        [" "," "," "," "," "," "],
+        [" ","|"," "," "," "," "],
+        [" ","|"," "," "," "," "],
+        [" ","|"," "," "," "," "],
+        [" ","|"," "," "," "," "]   ]
 for line in fileopen:
     ninety.append(line.strip())
 RC = random.choice(ninety)
@@ -34,6 +41,39 @@ while B == 1:
 
     if wrong == True:
         print(random.choice(w))
+        if d == 0:
+            hf[0][1] = "|"
+            hf[1][1] = "|"
+            hf[2][1] = "|"
+            hf[3][1] = "|"
+        elif d == 1:
+            hf[0][2] = "-"
+            hf[0][3] = "-"
+            hf[0][4] = "|"
+        elif d == 2:
+            hf[1][3] = "-"
+            hf[1][4] = "-"
+            hf[1][5] = "-"
+            hf[2][3] = "|"
+            hf[2][4] = " "
+            hf[2][5] = "|"
+            hf[3][3] = "-"
+            hf[3][4] = "-"
+            hf[3][5] = "-"
+        elif d == 3:
+            hf[4][4] = "|"
+            hf[5][4] = "|"
+            hf[6][4] = "|"
+        elif d == 4:
+            hf[5][3] = "-"
+        elif d == 5:
+            hf[5][5] = "-"
+        elif d == 6:
+            hf[7][3] = "/"
+        elif d == 7:
+            hf[7][5] = "\ "
+        for x in range(8):
+            print("".join(hf[x]))
         d = d + 1
         print("Failed : " + str(d))
     if d == 7:
