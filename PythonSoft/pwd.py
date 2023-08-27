c476f84f04fadf0ebf8a9da4402dc1dd2e73070c
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-08-27 15:17:04 +0100

    PWD - Now with advanced password generation

--- PythonSoft/pwd.py
+++ PythonSoft/pwd.py
@@ -1,16 +1,15 @@
 import random
 flag = True
 print("****THE OPEN SOURCE PASSWORD SECURITY SYSTEM****")
-print("Program version 1.2.1")
-print("The creator of this program is Okmeque1")
-
+print("Program version 1.2.0")
 while flag == True:
     print("Affiliate company © TCG - Technology, Coding and Gaming Inc ™")
     print("1 -> Add or generate password to save file.")
     print("2 -> Retrieve password from save file.")
     print("3 -> Format save file")
     print("4 -> More info.")
-    print("5 -> Save and quit")
+    print("5 -> Advanced password generator.")
+    print("6 -> Save and quit")
     print("When you are asked a valid file name,please make sure that the directory is valid and for best compatability,please make sure that the file already exists.")
     print("If you are a non-technical user,please choose option 4 before proceeding as it tells you about filepaths,OS compatability and more.")
     print("This is an open-source program so you can share it anywhere on the internet,USB/CD/DVD or other media.Please mention in your copy Okmeque1 so that the original code is not lost to time.")
@@ -20,13 +19,13 @@ while flag == True:
 
         print('Welcome to your password generator')
 
-        chars = '¦¬`1!23£4$€5%6^7&8*9(0-_=+qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlL;:@~#\|zZxXcCvVbBnNmMm,<.>/?'
+        chars = '¦¬`1!23£4$€5%6^7&8*9(0)-_=+qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlL;:@~#\|zZxXcCvVbBnNmMm,<.>/?'
 
         number = 1
 
         length = input('Password length ')
         length = int(length)
-
+        
 
 
         for pwd in range(number):
@@ -95,7 +94,7 @@ while flag == True:
         print("You may change the program defaults on line 37,39,53 and 55.(If you want to do this,you must change the defaults on ALL of the lines to ensure maximum compatability and change the input str to the default set in the none → default variable)")
         print("For maximum compatability,run this program in Python 3+ and Windows 7 or higher(Please note that you can run it lower than those versions but the program might throw errors in lower version of windows(XP,Vista,etc) and some python functions might not exist in lower versions of python)")
         print("The program will run on macOS and Linux but the filepath format will vary as neither of those use drive letters (eg A:\directory\file.ext).The structure for those OS's will either be : ")
-        print("1 : macOS : The file structure may be /path/path1/pwdfile.extention (Please note that this program might not work as macOS does NOT support harddisk writing to a bit level and might throw an error about write fail.)")
+        print("1 : macOS : The file structure may be /path/path1/pwdfile.extention")
         print("2 : Linux : The file structure is unclear as there are so many Linux distros out there but the structure may be /dev/sda/mountpoint1/folder/pwdfile.extention.")
         print("Please note that in both cases,DO NOT USE FOREIGN FILE EXTENTIONS(.dell,pc or any non-standard file format that can't be read by a text editor.) as the disk check utility might assume that the file is corrupt and delete it.")
         print("Please do NOT modify this program as the file may become unoriginal and might cause program breakage.This program took HOURS to complete and be at its current state.\n")
@@ -108,8 +107,43 @@ while flag == True:
         #passall = open("u:\PWD_OPENSCS.pwd","r")
         #print(passall)
         #passall.close()
-
     elif option == 5:
+        end = ""
+        passwd = ""
+        spc = '¦¬`!£$€%^&*()-_=+;:@~#\|,<.>/?'
+        num = "1234657890"
+        ch = "qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLzZxXcCvVbBnNmMm"
+        spcs = ""
+        nums = ""
+        chs = ""
+        spcn = int(input("Enter special character count : "))
+        numn = int(input("Enter number count : "))
+        chn = int(input("Enter standard character count : "))
+        for p in range(spcn):
+            spcs += random.choice(spc)
+        for s in range(numn):
+            nums += random.choice(num)
+        for ad in range(chn):
+            chs += random.choice(ch)
+        end += spcs + nums + chs
+        for x in range(len(end)):
+            passwd += random.choice(end)
+        print(passwd)
+        filenam = input("Please enter a valid file name (none to default of G:\python\demo\demo.pc). The format must be a:\directory\pwdfile.extention. : ")
+        if filenam == "":
+            filenam = "G:\python\demo\demo.pc"
+            sets = input("Enter a set name for your password to continue this program.This will be used later to retrieve back the password.This program does NOT support having 2 sets of the same name.Entering a name that already exists will cause a conflict. : ")
+            passavee = open(filenam,"a")
+            passavee.write(sets + " -> " + passwd + "\n")
+            passavee.close()
+        else:
+            sets = input("Enter a set name for your password to continue this program.This will be used later to retrieve back the password.This program does NOT support having 2 sets of the same name.Entering a name that already exists will cause a conflict. : ")
+            passavee = open(filenam,"a")
+            passavee.write(sets + " -> " + passwd + "\n")
+            passavee.close()
+        print("Save has completed with no disk errors.")
+        print("Now returning to the main menu")
+    elif option == 6:
         print("You have quit this program and you are now in a command processor.Please quit the command processor if you do not know or want to use it.")
         flag = False    
 
