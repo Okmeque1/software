9590d0084a6800e5f4cea1b0619a07ee0025ba4a
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-09-19 21:53:19 +0100

    Encrypter-Decrypter system - Encrypt/Decrypt Files

--- PythonSoft/Encrypter-Decrypter.py
+++ PythonSoft/Encrypter-Decrypter.py
@@ -8,7 +8,6 @@ def enc(key):
     with open(file_enc,"wb") as twrite:
         twrite.write(encrypted)
     print("Encrypted with no errors")
-    start()
 def dec(key):
     file_dec = input("Please enter a valid file name to decrypt → ")
     with open(file_dec,"rb") as lwd:
@@ -18,17 +17,24 @@ def dec(key):
     with open(file_dec,"wb") as td:
         td.write(decrypted)
     print("Decrypted with no errors.")
-    start()
 def start():
-    key = input("Please enter a valid file name with a valid key. : ")
-    print("Encrypter-Decripter system by Okmeque1")
-    print("1 → Encrypt file.")
-    print("2 → Decrypt file.")
-    a = input("Please choose : ")
-    if a == "1":
-        enc(key)
-    elif a == "2":
-        dec(key)
-    else:
-        return
+    try:
+        key = input("Please enter a valid file name with a valid key. : ")
+        print("Encrypter-Decrypter system by Okmeque1")
+        print("1 → Encrypt file.")
+        print("2 → Decrypt file.")
+        a = input("Please choose : ")
+        if a == "1":
+            enc(key)
+        elif a == "2":
+            dec(key)
+        else:
+            return
+    except FileNotFoundError:
+        print("STOP : 6510B\nFile specified does not exist.Make sure the file exists and try again.")
+        input("Press ENTER to exit.")
+        return None
+    except KeyboardInterrupt or EOFError:
+        print("STOP : 0250/0270\nUser has chosen to exit.Exiting...")
+        return None
 start()
