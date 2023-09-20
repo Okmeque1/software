da406690b34e61c66f3a70096e963e6ad7162c78
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-09-20 19:03:24 +0100

    Not 1 Game - See if you lose on first try

--- PythonSoft/not 1.py
+++ PythonSoft/not 1.py
@@ -4,43 +4,25 @@ num = random.choice([1,2,3,4,5,6])
 
 
 def dés(score):
-    num = random.choice([1,2,3,4,5,6])
-    score = score + num
-    print(num)
-    if num == 1:
-        print("You lost")
-        print("The score is " + str(score))
-        return
-    else:
-        print("This is a random based game.If the number is one,you lose.Do you want to continue?[Y,N]")
-        a = input("")
-        if a == "N":
-            print("The score is " + str(score) + ".The game has ended")
-        if a == "Y":
-            print("Score saved.You will see the score when you lose.")
-            dés(score)
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
- 
+    try:
+        num = random.choice([1,2,3,4,5,6])
+        score = score + num
+        print(num)
+        if num == 1:
+            print("You lost")
+            print("The score is " + str(score))
+            return
+        else:
+            print("This is a random based game.If the number is one,you lose.Do you want to continue?[Y,N]")
+            a = input("")
+            if a == "N":
+                print("The score is " + str(score) + ".The game has ended")
+            if a == "Y":
+                print("Score saved.You will see the score when you lose.")
+                dés(score)
+    except KeyboardInterrupt or EOFError:
+        print("STOP : 0250/0270\nUser has chosen to exit.Exiting...")
+        return None
 def start():
     print("NOT 1 GAME : The computer will choose a number.If the number is 1,you lose the game.")
     print("On easy mode,your score is not reset when you lose,on hard mode,the score is 0 if you lose.")
@@ -58,26 +40,25 @@ def start():
         print('Saving the game...')
         time.sleep(10)
         print('Game stopped.')
-              
-
-
-
 def dés1(score):
-             num = random.choice([1,2,3,4,5,6])
-             score = score + num
-             print(num)
-             print("The score is " + str(score))
-             if num == 1:
-                 print("You lost")
-                 print("The score is 0")
-                 return
-             else:
-                 print("This is a random based game.If the number is one,you lose.Do you want to continue?[Y,N]")
-                 a = input("")
-                 if a == "N":
-                     print("The score is " + str(score) + ".The game has ended")
-                 if a == "Y":
-                     
-                     dés1(score)
-                     
+             try:
+                num = random.choice([1,2,3,4,5,6])
+                score = score + num
+                print(num)
+                print("The score is " + str(score))
+                if num == 1:
+                    print("You lost")
+                    print("The score is 0")
+                    return
+                else:
+                    print("This is a random based game.If the number is one,you lose.Do you want to continue?[Y,N]")
+                    a = input("")
+                    if a == "N":
+                        print("The score is " + str(score) + ".The game has ended")
+                    if a == "Y":
+                        
+                        dés1(score)
+             except KeyboardInterrupt or EOFError:
+                 print("STOP : 0250/0270\nUser has chosen to exit.Exiting...")
+                 return None
 start()
