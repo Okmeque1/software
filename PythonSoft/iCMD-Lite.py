5e85b0161fc0ada70d0213ee64db427467926e1a
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2023-09-20 18:35:49 +0100

    iCMD-Lite - iCMD but more compact

--- PythonSoft/iCMD-Lite.py
+++ PythonSoft/iCMD-Lite.py
@@ -1,11 +1,16 @@
 import os
 def icmd():
-    print("iCMD-Lite - iCMD with limited features : core functionality.\nCopyright (c) Okmeque1 Corporation 2023-2025")
-    while True:
-        prompt = input("iCMD-Lite>")
-        if prompt == 'exit' or prompt == 'return':
-                return False
-        elif 'cd' in prompt[0:3]:
-                os.chdir(prompt[3:])
-        os.system(prompt)
+        try:
+                print("iCMD-Lite - iCMD with limited features : core functionality.\nCopyright (c) Okmeque1 Corporation 2023-2025")
+                while True:
+                        prompt = input("iCMD-Lite>")
+                        if prompt == 'exit' or prompt == 'return':
+                                return False
+                        elif 'cd' in prompt[0:3]:
+                                os.chdir(prompt[3:])
+                        os.system(prompt)
+        except BaseException:
+                print("STOP : 770A\n General Exception.Retry the operation")
+                input("Press ENTER to exit...")
+                return None
 icmd()
