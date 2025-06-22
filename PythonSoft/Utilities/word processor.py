80519dfea45544bea504198715c77d7ae0edff0d
gmaersot42 <162634737+GmaerSoft42@users.noreply.github.com>
2025-06-22 17:30:45 +0100

    Update word processor.py

--- PythonSoft/Utilities/word processor.py
+++ PythonSoft/Utilities/word processor.py
@@ -1,29 +1,40 @@
-def list_arr(a):
-    c = {}
-    print('This will tell you the position of the word and if it is in the file : ')
-    g = input("Enter word to choose,leave blank for full function : ")
-    if g == "":
-        for b in a:
-            if b not in c:
-                c[b] = 1
-            elif b in c:
-                c[b] = c[b] + 1
+def list_arr(array):
+    returndict = {}
+    print('This will tell you the position of the word and if it is in the file. ')
+    word = input("Enter word to choose, leave blank for full function : ")
+    if word == "":
+        for b in array:
+            if b not in returndict:
+                returndict[b] = 1
+            elif b in returndict:
+                returndict[b] = returndict[b] + 1
             
 
-    for b in a:
-        if g == b:
-            if b in c:
-                c[b] = c[b] + 1
-            elif b not in c:
-                c[b] = 1
-
-
-    return c
-def show_result(d):
-    for e in d:
-        print(e + ":" + str(d[e]))
+    for b in array:
+        if word == b:
+            if b in returndict:
+                returndict[b] = returndict[b] + 1
+            elif b not in returndict:
+                returndict[b] = 1
+    return returndict
+def show_result(file):
+    for word in file:
+        print(word + ":" + str(file[word]))
 def individual_words():
-    with open("C:/Users/dell/desktop/test.txt","r") as f:
-        return(f.read().split(" "))
-
-show_result(list_arr(individual_words()))
+    with open(input("Please enter a valid file name (the full path to the file must be specified if the file in question is not in the same directory as this file): ","r") as f:
+        return(f.read().split(" ").replace(","," ").replace(";"," ").replace(":"," ").replace("."," ").replace("!"," ").replace("?"," "))
+def start():
+    try:
+        show_result(list_arr(individual_words()))
+    except FileNotFoundError as e:
+        print(f"Error 6510B\n The file that you specified is not a valid file.\nDetails: {e}")
+        input("Press ENTER to restart the program, or press CTRL + C to END the program...")
+        start()
+    except (KeyboardInterrupt, SystemExit, EOFError):
+        print("User has chosen to exit. Exiting...")
+        exit()
+    except Exception as e:
+        print(f"Unknown error: {e}\nReview the error chart for more information as well as the Python Manual.")
+        input("Press ENTER to restart the program, or press CTRL + C to END the program...")
+        start()
+start()
